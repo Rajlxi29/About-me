3f59af70a3e2d94a74a13f644cbfa0a659900c5a
Nilesh Raj <158495100+Rajlxi29@users.noreply.github.com>
2025-07-27 23:10:52 +0530

    Add files via upload

---
+++ Portfolio/Project.js
@@ -0,0 +1,15 @@
+/*var typed = new Typed(".text", {
+    strings:["Data Scientist", "Machine Learning Engineer", "FrontendDev"],
+    typeSpeed: 1000,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+});*/
+
+var typed = new Typed(".text", {
+      strings: ["Data Scientist", "Machine Learning Engineer", "FrontEnd Dev"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true
+    });
